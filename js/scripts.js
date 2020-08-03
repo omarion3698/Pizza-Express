5b4a2c152063790f7aa32f604787055b90b56819
omarion3698 <omaribinbakarivic@gmail.com>
2020-08-03 23:47:33 +0300

    Add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
  
 var price , crust_price, topping_price ;
 let total = 0;
-function Getpizza( name,size,crust,topping, total ) {
+function Getpizza( name,size,crust,topping, total) {
   this.name = name;
   this.size = size;
   this.crust = crust;
@@ -10,17 +10,17 @@ function Getpizza( name,size,crust,topping, total ) {
 }
 
 $(document).ready(function() {
-   $("button.proceed").click(function() {
-     $("button.proceed").hide();
-     $("#information").hide();
-     $("div.choise").slideDown(1000);
-   });
+  $("button.proceed").click(function() {
+  $("button.proceed").hide();
+  $("#information").hide();
+  $("div.choise").slideDown(1000);
+  });
   $("button.proceed").click(function(event) {
    let pname = $(".name option:selected").val();
    let psize = $("#size option:selected").val();
    let pcrust = $("#crust option:selected").val();
    let ptopping = [];
-   $.each($("input[name='toppings']:checked"), function() {            
+   $.each($("input[name='toppings']:checked"), function() {        
        ptopping.push($(this).val());
    });
    console.log(ptopping.join(", "));
@@ -59,7 +59,6 @@ $(document).ready(function() {
       default:
         console.log("No price"); 
     }
-
     let topping_value = ptopping.length*50;
     console.log("toppins value" + topping_value);
 
@@ -87,12 +86,12 @@ $(document).ready(function() {
     $("#pizzatopping").html(ptopping.join(", "));
     $("#totals").html(total);
     
-    $("button.addPizza").click(function() {
+    $("button.addPizza").click(function(){
       let pname = $(".name option:selected").val();
       let psize = $("#size option:selected").val();
       let pcrust = $("#crust option:selected").val();
       let ptopping = [];
-      $.each($("input[name='toppings']:checked"), function(){            
+      $.each($("input[name='toppings']:checked"), function() {    
           ptopping.push($(this).val());
       });
       console.log(ptopping.join(", "));
@@ -130,7 +129,6 @@ $(document).ready(function() {
           default:
             console.log("No price"); 
         }
-        
         let topping_value = ptopping.length*50;
         console.log("toppins value" + topping_value);
         total = price + crust_price + topping_value;
@@ -138,14 +136,12 @@ $(document).ready(function() {
 
         checkoutTotal = checkoutTotal + total;
         console.log(checkoutTotal);
-
       var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
 
       $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
       console.log(newOrder);
-
+      
     });
-
     $("button#checkout").click(function() { 
       $("button#checkout").hide();
       $("button.addPizza").hide();
@@ -179,7 +175,7 @@ $(document).ready(function() {
       let phone = $("input#phone").val();
       let location = $("input#location").val();
 
-      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!="") {
+      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
   
         $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliceryamount);
         $("#totalbill").hide();
